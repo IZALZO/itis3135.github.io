2bab8ce5d2ba0631952269495948305c3f940c8e
IZALZO <rmauney6@uncc.edu>
2022-02-27 21:48:08 -0500

    c

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,66 @@
+/**So I know how to do prompt the user and get the numbers from the buttons but i did not know how to get it to appear on the screen
+ * without a large amount of work so i gave up here
+*/
+function addition()
+{
+    var x;
+    var y;
+    var result;
+
+    x = prompt("First Number"); 
+    x = parseFloat(x);
+
+    y = prompt("Second Number"); 
+    y = parseFloat(y); 
+    
+    result = x - y; 
+    alert(x + " + " + y + " = " + result);
+}
+
+function subtraction()
+{
+    var x;
+    var y;
+    var result;
+
+    x = prompt("First Number"); 
+    x = parseFloat(x);
+
+    y = prompt("Second Number"); 
+    y = parseFloat(y); 
+    
+    result = x - y; 
+    alert(x + " - " + y + " = " + result);
+}
+
+function multiplication()
+{
+    var x;
+    var y;
+    var result;
+
+    x = prompt("First Number"); 
+    x = parseFloat(x);
+
+    y = prompt("Second Number"); 
+    y = parseFloat(y); 
+    
+    result = x - y; 
+    alert(x + " * " + y + " = " + result);
+}
+
+function division()
+{
+    var x;
+    var y;
+    var result;
+
+    x = prompt("First Number"); 
+    x = parseFloat(x);
+
+    y = prompt("Second Number"); 
+    y = parseFloat(y); 
+    
+    result = x - y; 
+    alert(x + " / " + y + " = " + result);
+}
