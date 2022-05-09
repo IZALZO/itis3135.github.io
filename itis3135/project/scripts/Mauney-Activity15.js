63a7ea60570434cc62804e7aacf8ae37b729e156
IZALZO <rmauney6@uncc.edu>
2022-05-09 18:49:01 -0400

    c

--- itis3135/project/scripts/Mauney-Activity15.js
+++ itis3135/project/scripts/Mauney-Activity15.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
             $.each(data, function()
                 {
                     $.each(this, function(key, value){
-                        $("#faculty").append('<img src="' + value.image + '">');
                         $("#faculty").append('<h2>' + value.full_name + '</h2>');
                         $("#faculty").append('<h3>' + value.department + '</h3>');
                         $("#faculty").append('<p>' + value.bio + '</p>');
